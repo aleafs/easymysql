5d1a2262dd4dea015a5df6653a7dc6c9bd525d67
aleafs <zhangxc83@gmail.com>
2012-10-31 10:49:46 +0800

    update unit test

--- test/mysql.test.js
+++ test/mysql.test.js
@@ -30,9 +30,12 @@ describe('mysql pool', function () {
       done();
     });
 
-    _me.query('SELECT SLEEP(0.06) AS a', 50, function (error, res) {
-      should.ok(!res);
-      error.should.have.property('name', 'QueryTimeout');
+    _me.query('SELECT 1', function (error, res) {
+      should.ok(!error);
+      _me.query('SELECT SLEEP(0.06) AS a', 50, function (error, res) {
+        should.ok(!res);
+        error.should.have.property('name', 'QueryTimeout');
+      });
     });
   });
   /* }}} */
@@ -43,15 +46,19 @@ describe('mysql pool', function () {
     var _me = Mysql.create({
       'maxconnection' : 2
     });
-    _me.on('error', function (error) {
+    _me.on('error', function (error) {});
+    _me.on('notice', function (message) {
+      //console.log(message);
     });
     _me.addserver(config);
+    _me.addserver(config);  // XXX: 失效,混测试覆盖率
 
     /**
      * @连接不同的机器 
      */
     _me.addserver({
       'host'  : '1.1.1.1',
+      'port'  : 3306,
       'user'  : 'root',
       'password'  : ''
     });
