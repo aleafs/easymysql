737667366969115cf6c881d7e5551cdc51bf2723
aleafs <zhangxc83@gmail.com>
2012-11-17 18:37:18 +0800

    emit error when conn.on('error')

--- test/connection.test.js
+++ test/connection.test.js
@@ -7,10 +7,10 @@ var interceptor = require('interceptor');
 var Common = require(__dirname + '/common.js');
 
 var Connection = require(__dirname + '/../lib/connection.js');
-var getBlocker = function (port) {
+var getBlocker = function (port, cb) {
   var cfg = Common.extend();
   var _me = interceptor.create(util.format('%s:%d', cfg.host, cfg.port || 3306));
-  _me.listen(port);
+  _me.listen(port, cb);
   return _me;
 };
 
@@ -65,62 +65,68 @@ describe('mysql connection', function () {
 
   /* {{{ should_connect_timeout_works_fine() */
   it('should_connect_timeout_works_fine', function (done) {
-    var blocker = getBlocker(33061);
-    var _config = Common.extend({
-      'host' : 'localhost', 'port' : 33061
-    });
+    var blocker = getBlocker(33061, function () {
+      var _config = Common.extend({
+        'host' : 'localhost', 'port' : 33061
+      });
 
-    blocker.block();
-    var _me = Connection.create(_config);
-    _me.on('error', function (e) {
-      e.should.have.property('name'/*, 'ConnectTimeout'*/);
-      e.message.should.include(getAddress(_config));
-      blocker.close();
-      _me.close(done);
+      blocker.block();
+      var _me = Connection.create(_config);
+      _me.on('error', function (e) {
+        e.should.have.property('name'/*, 'ConnectTimeout'*/);
+        e.message.should.include(getAddress(_config));
+        blocker.close();
+        _me.close(done);
+      });
     });
   });
   /* }}} */
 
   /* {{{ should_got_server_restart_event() */
   it('should_got_server_restart_event', function (done) {
-
-    var blocker = getBlocker(33061);
-    var _config = Common.extend({
-      'host' : 'localhost', 'port' : 33061
-    });
-
-    blocker.open();
-    var _me = Connection.create(_config);
-
-    var _events = [];
-    ['error', 'close'].forEach(function (i) {
-      _me.on(i, function (e) {
-        if (e) {
-          e.message.should.include(getAddress(_config));
-        }
-        _events.push([i].concat(Array.prototype.slice.call(arguments, 0)));
+    var blocker = getBlocker(33061, function () {
+      var _config = Common.extend({
+        'host' : 'localhost', 'port' : 33061
       });
-    });
 
-    _me.query('SHOW DATABASES', 100, function (error, res) {
-      should.ok(!error);
-      res.should.includeEql({'Database' : 'mysql'});
+      blocker.open();
+      var _me = Connection.create(_config);
 
-      /**
-       * XXX: server 端关闭
-       */
-      _events = [];
-      blocker.close();
-
-      setTimeout(function () {
-        _events.should.eql([['close']]);
-        _me.query('SHOW DATABASES', 100, function (error, res) {
-          should.ok(error);
-          //error.should.have.property('code', 'PROTOCOL_CONNECTION_LOST');
-          error.message.should.include(getAddress(_config));
-          _me.close(done);
+      var _events = [];
+      ['error', 'close'].forEach(function (i) {
+        _me.on(i, function (e) {
+          if (e) {
+            e.message.should.include(getAddress(_config));
+          }
+          _events.push([i].concat(Array.prototype.slice.call(arguments, 0)));
         });
-      }, 20);
+      });
+
+      _me.query('SHOW DATABASES', 100, function (error, res) {
+        should.ok(!error);
+        res.should.includeEql({'Database' : 'mysql'});
+        var afterClosed = function () {
+          _events.should.eql([[
+            'error', {'fatal' : true, 'code' : 'PROTOCOL_CONNECTION_LOST', 'name' : 'MysqlError'}
+            ], ['close']]);
+          _me.query('SHOW DATABASES', 100, function (error, res) {
+            should.ok(error);
+            error.should.have.property('code', 'PROTOCOL_ENQUEUE_AFTER_QUIT');
+            error.message.should.include(getAddress(_config));
+            _me.close(done);
+          });
+        };
+
+        blocker.outArr[0].once('close', function () {
+          setTimeout(afterClosed, 20);
+        });
+
+        /**
+         * XXX: server 端关闭
+         */
+        _events = [];
+        blocker.close();
+      });
     });
   });
   /* }}} */
