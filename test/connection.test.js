26bd6457e8e864ee8501ed845a94f5f74888ff07
zhaolei <iseeyou1987@sina.com>
2012-12-24 13:26:33 +0800

    remove console.log

--- test/connection.test.js
+++ test/connection.test.js
@@ -34,7 +34,6 @@ describe('mysql connection', function () {
     _me.query('SELECT SLEEP(0.01)', 0, function (e, r) {
       should.ok(!e);
       (Date.now() - now).should.above(9);
-      console.log(r);
       r.should.includeEql({'SLEEP(0.01)' : 0});
     });
 
